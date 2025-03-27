70ffd2364d4b9fce25083607098be7332efde17c
reighnman <jaredo@ameritech.net>
2025-03-26 20:38:39 -0500

    update for nexus

--- src/SubspaceStats/Models/GameType.cs
+++ src/SubspaceStats/Models/GameType.cs
@@ -2,20 +2,26 @@
 {
     public enum GameType : long
     {
-        SVS_1v1 = 1,
-        SVS_2v2 = 2,
-        SVS_3v3 = 3,
-        SVS_4v4 = 4,
-        PowerBall_Traditional = 5,
-        PowerBall_Proball = 6,
-        PowerBall_Smallpub = 7,
-        PowerBall_3h = 8,
-        PowerBall_small4tmpb = 9,
-        PowerBall_minipub = 10,
-        PowerBall_mediumpub = 11,
-        SVS_4v4League = 12,
-        SVS_SoloFFA = 13,
-        SVS_TeamFFA_2s = 14
+        Nexus_Duel = 1,
+        Nexus_1v1_Obstacle = 2,
+        Nexus_2v2_Box = 3,
+        Nexus_2v2_Obstacle = 4,
+        Nexus_3v3_Obstacle = 5,
+        Nexus_4v4_Obstacle = 6,
+        SVS_4v4_League = 7,
+        SVS_4v4_Practice = 8,
+        SVS_4v4_MatchMaking = 9,
+        SVS_SoloFFA = 10,
+        SVS_TeamFFA_2s = 11,
+        PowerBall_Traditional = 12,
+        PowerBall_Proball = 13,
+        PowerBall_Smallpub = 14,
+        PowerBall_3h = 15,
+        PowerBall_small4tmpb = 16,
+        PowerBall_minipub = 17,
+        PowerBall_mediumpub = 18,
+        Chaos = 19,
+
     }
 
     public enum GameCategory
@@ -31,13 +37,17 @@
         {
             return gameType switch
             {
-                GameType.SVS_1v1 or
-                GameType.SVS_SoloFFA => GameCategory.Solo,
+                GameType.Nexus_Duel or
+                GameType.Nexus_1v1_Obstacle => GameCategory.Solo,
 
-                GameType.SVS_2v2 or
-                GameType.SVS_3v3 or
-                GameType.SVS_4v4 or
-                GameType.SVS_4v4League or
+                GameType.Nexus_2v2_Box or
+                GameType.Nexus_2v2_Obstacle or
+                GameType.Nexus_3v3_Obstacle or
+                GameType.Nexus_4v4_Obstacle or
+                GameType.SVS_4v4_League or
+                GameType.SVS_4v4_Practice or
+                GameType.SVS_4v4_MatchMaking or
+                GameType.SVS_SoloFFA or
                 GameType.SVS_TeamFFA_2s => GameCategory.TeamVersus,
 
                 GameType.PowerBall_Traditional or
@@ -56,10 +66,17 @@
         {
             return gameType switch
             {
-                GameType.SVS_1v1 => "1v1",
-                GameType.SVS_2v2 => "2v2",
-                GameType.SVS_3v3 => "3v3",
-                GameType.SVS_4v4 => "4v4",
+                GameType.Nexus_Duel => "Nexus Public - Duel (box)",
+                GameType.Nexus_1v1_Obstacle => "Nexus Public - 1v1 (obstacle)",
+                GameType.Nexus_2v2_Box => "Nexus Public - 2v2 (box)",
+                GameType.Nexus_2v2_Obstacle => "Nexus Public - 2v2 (obstacle)",
+                GameType.Nexus_3v3_Obstacle => "Nexus Public - 3v3 (obstacle)",
+                GameType.Nexus_4v4_Obstacle => "Nexus Public - 4v4 (obstacle)",
+                GameType.SVS_4v4_League => "SVS 4v4 League ",
+                GameType.SVS_4v4_Practice => "SVS 4v4 Practice",
+                GameType.SVS_4v4_MatchMaking => "SVS 4v4 MatchMaking",
+                GameType.SVS_SoloFFA => "SVS Solo FFA",
+                GameType.SVS_TeamFFA_2s => "SVS Team FFA 2s",
                 GameType.PowerBall_Traditional => "Powerball - Traditional",
                 GameType.PowerBall_Proball => "Powerball - Proball",
                 GameType.PowerBall_Smallpub => "Powerball - Smallpub",
@@ -67,9 +84,7 @@
                 GameType.PowerBall_small4tmpb => "Powerball- small4tmpb",
                 GameType.PowerBall_minipub => "Powerball - minipub",
                 GameType.PowerBall_mediumpub => "Powerball - mediumpub",
-                GameType.SVS_4v4League => "SVS 4v4 League",
-                GameType.SVS_SoloFFA => "SVS Solo FFA",
-                GameType.SVS_TeamFFA_2s => "SVS Team FFA 2s",
+
                 _ => gameType.ToString(),
             };
         }
